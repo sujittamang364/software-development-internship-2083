7280c23b2824fa3f14b05eafcf395a5d90794e6b
Sujit Thokar <sujitthokar@Sujits-MacBook-Air.local>
2026-06-18 11:47:28 +0545

    intern_day2 file

---
+++ intern_day2.js
@@ -0,0 +1,12 @@
+const student = {
+    name: "Sujit Thokar",
+    age: 22,
+    semester:"7th",
+    college:"Hetauda City College",
+    skills:["HTML","CSS","JavaScript","React","Figma"],
+};
+console.log("Name:",student.name);
+console.log("Age:",student.age);
+console.log("Semester:",student.semester);
+console.log("College:",student.college);
+console.log("Skills:",student.skills);
